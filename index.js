2daee47ed460a9386c2f15f65c05e59f42424023
HarshKhandeparkar <mjkgoa@gmail.com>
2020-08-01 19:21:49 +0530

    fix: add compatibility for ws.on method

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 const { runHelper } = require('gpujs-hive-compute/dist/util/runHelper');
 
+class WSocket extends WebSocket {
+  constructor(...args) {
+    super(...args);
+  }
+
+  on(event, handler) {
+    console.log(event, handler, this)
+    switch (event) {
+      case 'open':
+        return this.onopen(handler);
+      case 'error':
+        return this.onerror(handler);
+      case 'message':
+        return this.onmessage(handler);
+      case 'close':
+        return this.onclose(handler);
+    }
+  }
+}
+
 function hiveHelp(gpu, url, logFunction = console.log) {
-  runHelper(WebSocket, gpu, url, logFunction)
+  runHelper(WSocket, gpu, url, logFunction);
+}
+
+module.exports = {
+  hiveHelp
 }

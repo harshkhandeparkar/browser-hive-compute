951c6b2942dff713ec6d6fd62a243e7c936bd5ef
HarshKhandeparkar <mjkgoa@gmail.com>
2020-10-24 13:13:50 +0530

    demo: slight changes

--- demo/index.js
+++ demo/index.js
@@ -3,14 +3,14 @@ const logList = document.querySelector('#logs ul');
 
 const customLog = (...logs) => {
   const li = document.createElement('li');
-  li.innerHTML = `<li><b>${new Date().toTimeString()}</b>: ${JSON.stringify(logs)}</li>`;
+  li.innerHTML = `<li><b>${new Date().toTimeString()}</b>: ${logs.toString()}</li>`;
   logList.append(li);
 }
 
 document.querySelector('#url-form').addEventListener('submit', e => {
   e.preventDefault();
   const url = document.querySelector('#url-input').value;
-  
+
   try {
     GPUjsHiveCompute.hiveHelp({
       gpu,
@@ -23,4 +23,12 @@ document.querySelector('#url-form').addEventListener('submit', e => {
   catch (e) {
     console.log('An error occured', e);
   }
+})
+
+document.querySelector('#restart').addEventListener('click', e => {
+  e.preventDefault();
+  logList.innerHTML = '';
+
+  document.querySelector('#url-form').classList.remove('invisible');
+  document.querySelector('#logs').classList.add('invisible');
 })
